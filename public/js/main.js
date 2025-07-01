a4171a9265bb0462743b5bf87dc71bbed4d652a8
nawhgnues <seungdang4@naver.com>
2025-07-01 16:09:22 +0900

    Submit Form to API

---
+++ public/js/main.js
@@ -0,0 +1,53 @@
+const output = document.querySelector("#output");
+const button = document.querySelector("#get-posts-btn");
+const form = document.querySelector("#add-post-form");
+
+// Get and show posts
+async function showPosts() {
+  try {
+    const res = await fetch("http://localhost:8080/api/posts");
+    if (!res.ok) {
+      throw new Error("Failed to fetch posts");
+    }
+
+    const posts = await res.json();
+    output.innerHTML = "";
+
+    posts.forEach((post) => {
+      const postEl = document.createElement("div");
+      postEl.textContent = post.title;
+      output.appendChild(postEl);
+    });
+  } catch (error) {
+    console.log("Error fetching posts: ", error);
+  }
+}
+
+// Submit new post
+async function addPost(e) {
+  e.preventDefault();
+  const formData = new FormData(this);
+  console.log(formData);
+  const title = formData.get("title");
+  try {
+    const res = await fetch("http://localhost:8080/api/posts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title }),
+    });
+
+    showPosts();
+
+    if (!res.ok) {
+      throw new Error("Failed to add post");
+    }
+  } catch (error) {
+    console.log("Error adding post");
+  }
+}
+
+// Event Listeners
+button.addEventListener("click", showPosts);
+form.addEventListener("submit", addPost);
--- server.js
+++ server.js
@@ -3,6 +3,11 @@ import posts from "./routes/posts.js";
 import logger from "./middleware/logger.js";
 import errorHandler from "./middleware/error.js";
 import notFound from "./middleware/notFound.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 8000;
@@ -13,6 +18,9 @@ app.use(logger);
 // Body parse middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// Setup static folder
+app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
 app.use("/api/posts", posts);
